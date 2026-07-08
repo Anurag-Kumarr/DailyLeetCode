a3f07ebcbf6511e432f7bbfbf0c30a8453add42c
Anurag <135734306+Anurag-Kumarr@users.noreply.github.com>
2026-07-08 14:01:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (35.86%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    int[] a=new int[46];
+    public int climbStairs(int n) {
+        if(n==1){
+            return 1;
+        }
+        if(n==2){
+            return 2;
+        }
+        if(a[n]!=0){
+            return a[n];
+        }
+        a[n]=climbStairs(n-1)+climbStairs(n-2);
+        return a[n];
+    }
+}
